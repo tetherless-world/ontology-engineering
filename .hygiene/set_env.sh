4c89b600bb14a7b7123aa40c84d034cad3584233
solashirai <solashakashirai@gmail.com>
2022-10-31 14:22:11 -0400

    set main tests to curated_tests

--- .hygiene/set_env.sh
+++ .hygiene/set_env.sh
@@ -18,7 +18,7 @@ HYGIENE_WORKSPACE=${HYGIENE_WORKSPACE:-"$(cd "${REPO_DIR}" && cd .. && pwd)"}
 HYGIENE_NAMESPACE_REGEX="^https:\/\/tw\.rpi\.edu\/ontology-engineering\/"
 
 # Relative path to the test directory
-HYGIENE_TEST_SUBDIR=".hygiene/curated_tests_some_errors"
+HYGIENE_TEST_SUBDIR=".hygiene/curated_tests"
 
 # If true, fail tests that produce warnings
 HYGIENE_FAIL_ON_WARNINGS=false
