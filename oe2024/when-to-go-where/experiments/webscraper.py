685d6f5f4c357bf28907264d20de80275d6f4c6f
Samyuth <samyuth.sagi@gmail.com>
2024-11-07 14:46:23 -0500

    Adding hike individuals

--- oe2024/when-to-go-where/experiments/webscraper.py
+++ oe2024/when-to-go-where/experiments/webscraper.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+def convert_to_camel_case(name):
+    # Remove non-alphanumeric characters
+    name = re.sub(r'[^A-Za-z0-9 ]+', '', name)
+    # Split by spaces, capitalize each word, and join without spaces
+    return ''.join(word.capitalize() for word in name.split())
 
 LINK = "https://www.cntraveler.com/gallery/the-best-hike-in-every-national-park"
 
@@ -22,34 +27,42 @@ for figure in figures:
     children = list(paragraph.children)
 
     name = children[1].strip()
-    hike_len = " ".join(children[3].split()[1:]).strip()
+    hike_len = children[3].split()[1].strip()
     hike_difficuty = children[5].split()[-1].strip()
 
     parks.append(park)
-    print(park)
-    print(name)
-    print(hike_len)
-    print(hike_difficuty)
+    park = convert_to_camel_case(park)
+    name = convert_to_camel_case(name)
+
+    # print(park)
+    # print(name)
+    # print(hike_len)
+    # print(hike_difficuty)
+
+    xml_element = f"""
+    <owl:NamedIndividual rdf:about="&oe-wtgw-ind;{name}">
+        <rdf:type rdf:resource="&oe-wtgw;Hike"/>
+        <oe-wtgw:hasDifficulty rdf:resource="&oe-wtgw-ind;{hike_difficuty}"/>
+        <oe-wtgw:hasDistance rdf:datatype="&xsd;decimal">{hike_len}</oe-wtgw:hasDistance>
+        <oe-wtgw:isHikeOf rdf:resource="&oe-wtgw-ind;{park}"/>
+    </owl:NamedIndividual>
+    """
+
+    print(xml_element)
     print()
 
-print(parks)
+# print(parks)
 # List of park names
 parks = ["BigBend", "Yellowstone", "Yosemite", "GrandCanyon"]
 
-def convert_to_camel_case(name):
-    # Remove non-alphanumeric characters
-    name = re.sub(r'[^A-Za-z0-9 ]+', '', name)
-    # Split by spaces, capitalize each word, and join without spaces
-    return ''.join(word.capitalize() for word in name.split())
-
 camel_case_parks = [convert_to_camel_case(park) for park in parks]
 
-# Add each park as an individual
-for original_name, camel_case_name in zip(parks, camel_case_parks):
-    xml_element = f"""
-    <owl:NamedIndividual rdf:about="https://tw.rpi.edu/ontology-engineering/oe2024/when-to-go-where/WhenToGoWhere-individuals/{camel_case_name}">
-        <rdf:type rdf:resource="https://tw.rpi.edu/ontology-engineering/oe2024/when-to-go-where/WhenToGoWhere/NationalPark"/>
-        <rdfs:label>{camel_case_name}</rdfs:label>
-    </owl:NamedIndividual>
-    """
-    print(xml_element)
+# # Add each park as an individual
+# for original_name, camel_case_name in zip(parks, camel_case_parks):
+#     xml_element = f"""
+#     <owl:NamedIndividual rdf:about="https://tw.rpi.edu/ontology-engineering/oe2024/when-to-go-where/WhenToGoWhere-individuals/{camel_case_name}">
+#         <rdf:type rdf:resource="https://tw.rpi.edu/ontology-engineering/oe2024/when-to-go-where/WhenToGoWhere/NationalPark"/>
+#         <rdfs:label>{camel_case_name}</rdfs:label>
+#     </owl:NamedIndividual>
+#     """
+#     print(xml_element)
